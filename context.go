d8c644bc7a9797ed5e54a9c85bea76b72ebfa919
Mark Gomersbach <markgomersbach@gmail.com>
2023-12-22 15:29:36 +0100

    Add connection to context and move to private

--- context.go
+++ context.go
@@ -1,17 +1,21 @@
 package telnet
 
+import (
+	"net"
+)
 
 type Context interface {
 	Logger() Logger
+	Conn() net.Conn
 
 	InjectLogger(Logger) Context
+	InjectConn(net.Conn) Context
 }
-
 
 type internalContext struct {
 	logger Logger
+	conn   net.Conn
 }
-
 
 func NewContext() Context {
 	ctx := internalContext{}
@@ -19,13 +23,22 @@ func NewContext() Context {
 	return &ctx
 }
 
-
 func (ctx *internalContext) Logger() Logger {
 	return ctx.logger
 }
 
 func (ctx *internalContext) InjectLogger(logger Logger) Context {
 	ctx.logger = logger
+
+	return ctx
+}
+
+func (ctx *internalContext) Conn() net.Conn {
+	return ctx.conn
+}
+
+func (ctx *internalContext) InjectConn(conn net.Conn) Context {
+	ctx.conn = conn
 
 	return ctx
 }
